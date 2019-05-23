2711dfe00948a9474579f7a3d8087d9400a927df
LoganPM <logan@DESKTOP-NC5T4VP.localdomain>
2019-05-23 14:44:53 -0700

    table can clear

--- app/public/js/drawTables.js
+++ app/public/js/drawTables.js
@@ -1,6 +1,6 @@
 
 //Globals
-let tableArray = [];
+let combList = [];
 let table = null;
 let tableCounter = 0;
 
@@ -19,26 +19,47 @@ const renderTable = () => {
   // set headers
   tableHead.innerHTML = `
     <tr>
+		<th scope="col">Name</th>
         <th scope="col">Initative</th>
         <th scope="col">HP</th>
-        <th scope="col">Armor Class</th>
+        <th scope="col">AC</th>
     </tr>`;
+  //set table body
+  for (let i = 0; i < numRows; i++) {
+    const newRow = tableBody.insertRow(i);
+    newRow.innerHTML = `
+            <td>${combList[i].name}</td>
+			<td>${combList[i].init}</td>
+            <td>${combList[i].HP}</td>
+            <td>${combList[i].AC}</td>`;
+  }
 };
 
 document.getElementById('addNewBtn').onclick = () => {
   // re-render table
+  let name = document.getElementById('stats-name').value;
   let HP = document.getElementById('stats-hp').value;
   let init = document.getElementById('stats-init').value;
   let AC = document.getElementById('stats-ac').value;
-  let newComb = new newCombatant(HP,AC,init);
-  tableCounter++;
+  let newComb = new newCombatant(name,HP,AC,init);
   console.log(newComb);
+  combList[tableCounter] = newComb;
+  tableCounter++;
   renderTable();
   console.log('Updated!');
 };
 
+document.getElementById('clearTableBtn').onclick = () => {
+  for(let i = 0; i<tableCounter; i++){
+    combList[i] = null;
+  }
+  tableCounter = 0;
+  console.log("Table cleared")
+  renderTable();
+}
 document.getElementById('clearBtn').onclick = () => {
 
+  document.getElementById('stats-name').value = '';
   document.getElementById('stats-hp').value = '';
   document.getElementById('stats-init').value = '';
   document.getElementById('stats-ac').value = '';
@@ -46,7 +67,8 @@ document.getElementById('clearBtn').onclick = () => {
   console.log('Cleared');
 };
 
-let newCombatant = function(HP,AC,init){
+let newCombatant = function(name,HP,AC,init){
+	this.name = name
 	this.HP = HP
 	this.AC = AC
 	this.init = init
