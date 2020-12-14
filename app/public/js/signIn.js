45af41869c346534cffe1a922bb350c4f5a94e01
LoganPM <logan@DESKTOP-NC5T4VP.localdomain>
2020-12-14 17:53:47 -0500

    added buttons to main page & changed the layout, added Signin page/ signup, need to work on functionality

---
+++ app/public/js/signIn.js
@@ -0,0 +1,6 @@
+document.getElementById('login_btn').onclick = () => {
+    console.log("test\n");
+    let username = document.getElementById('unameSI').value;
+    let psswrd = document.getElementById('pswrdSI').value;
+    console.log(username + " " +psswrd + "\n");
+}
