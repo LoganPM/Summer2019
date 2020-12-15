d2f6cab299f817d5b5fc767ea51fb686b3960ea7
LoganPM <logan@DESKTOP-NC5T4VP.localdomain>
2020-12-15 18:44:38 -0500

    added a bunch of stiff, snack messages in signin page, sortable table, notepad, bone code for rotatble table

--- app/public/js/drawTables.js
+++ app/public/js/drawTables.js
@@ -2,17 +2,25 @@
 //Globals
 let combList = [];
 let table = null;
-let tableCounter = 0;
 
 const tableHead = document.getElementById('table-head');
 const tableBody = document.getElementById('table-body');
+
+let newCombatant = function(name,hp,ac,init, inmod){
+	this.name = name;
+	this.hp = hp;
+	this.ac = ac;
+  this.init = init;
+  this.inmod = inmod;
+}
 
 $(document).ready(function() {
       renderTable();
 });
 
+//DRAWS THE TABLE
 const renderTable = () => {
-  let numRows = tableCounter;
+  let numRows = combList.length;
   // reset table
   tableHead.innerHTML = '';
   tableBody.innerHTML = '';
@@ -21,6 +29,7 @@ const renderTable = () => {
     <tr>
 		<th scope="col">Name</th>
         <th scope="col">Initative</th>
+        <th scope="col">Initative Mod</th>
         <th scope="col">HP</th>
         <th scope="col">AC</th>
     </tr>`;
@@ -29,34 +38,70 @@ const renderTable = () => {
     const newRow = tableBody.insertRow(i);
     newRow.innerHTML = `
             <td>${combList[i].name}</td>
-			      <td>${combList[i].init}</td>
-            <td>${combList[i].HP}</td>
-            <td>${combList[i].AC}</td>`;
+            <td>${combList[i].init}</td>
+            <td>${combList[i].inmod}</td>
+            <td>${combList[i].hp}</td>
+            <td>${combList[i].ac}</td>`;
   }
 };
+//SORTS THE TABLE
+function sortTable() {
+  let len = combList.length - 1;
+  let swap;
 
+  do {
+    swap = false;
+    for(let i=0; i<len; i++) {
+      if(combList[i].init < combList[i+1].init) {
+        let temp = new newCombatant(" "," "," "," ");
+        //store combj
+        temp.name = combList[i].name;
+        temp.hp = combList[i].hp;
+        temp.init = combList[i].init;
+        temp.ac = combList[i].ac;
+        temp.inmod = combList[i].inmod;
+        // swap combj with next element
+        combList[i].name = combList[i+1].name;
+        combList[i].ac = combList[i+1].hp;
+        combList[i].init = combList[i+1].init;  
+        combList[i].ac = combList[i+1].ac;
+        combList[i].inmod = combList[i+1].inmod;
+        // set next element to combj
+        combList[i+1].name = temp.name;
+        combList[i+1].hp = temp.hp;
+        combList[i+1].init = temp.init;
+        combList[i+1].ac = temp.ac;
+        combList[i+1].inmod = twmp.inmod;
+        swap = true;
+      }
+    }
+    len--;
+  } while (swap);
+}
+//ADD NEW ELEMENT TO TABLE BUTTON
 document.getElementById('addNewBtn').onclick = () => {
   // re-render table
   let name = document.getElementById('stats-name').value;
-  let HP = document.getElementById('stats-hp').value;
+  let hp = document.getElementById('stats-hp').value;
   let init = document.getElementById('stats-init').value;
-  let AC = document.getElementById('stats-ac').value;
-  let newComb = new newCombatant(name,HP,AC,init);
-  console.log(newComb);
-  combList[tableCounter] = newComb;
-  tableCounter++;
+  let ac = document.getElementById('stats-ac').value;
+  let inmod = document.getElementById('stats-inmod').value;
+  let newComb = new newCombatant(name,hp,ac,init,inmod);
+  combList.push(newComb);
+  sortTable();
   renderTable();
-  console.log('Updated!');
 };
 
+//CLEAR TABLE
 document.getElementById('clearTableBtn').onclick = () => {
-  for(let i = 0; i<tableCounter; i++){
-    combList[i] = null;
+  for(let i = 0; i< combList.length; i++){
+    combList[i].pop();
   }
-  tableCounter = 0;
-  console.log("Table cleared")
+  console.log("Table cleared");
   renderTable();
 }
+
+//CLEAR STATS
 document.getElementById('clearBtn').onclick = () => {
 
   document.getElementById('stats-name').value = '';
@@ -66,18 +111,11 @@ document.getElementById('clearBtn').onclick = () => {
   
   console.log('Cleared');
 };
-
-let newCombatant = function(name,HP,AC,init){
-	this.name = name
-	this.HP = HP
-	this.AC = AC
-	this.init = init
-}
-
+// SHOWS DROPDOWN FOR SAVE GAMES
 function showDropDown(){
   document.getElementById('state-dropdown').classList.toggle("show");
 }
-
+//for dropdown menu
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     let dropdowns = document.getElementsByClassName("dropdown-content");
@@ -88,5 +126,24 @@ window.onclick = function(event) {
         openDropdown.classList.remove('show');
       }
     }
+  }
+}
+
+document.getElementById('PrevTurnBtn').onclick = () => {
+  rotateTable(0);
+}
+
+document.getElementById('NextTurnBtn').onclick = () => {
+  rotateTable(1);
+}
+
+function rotateTable(direction) {
+  if (direction == 1){
+    // go next
+    let temp  = new newCombatant("","","","");
+    temp.name = combList[0];
+  }
+  else {
+    //go back
   }
 }
--- app/public/js/signIn.js
+++ app/public/js/signIn.js
@@ -1,12 +1,18 @@
 document.getElementById('login_btn').onclick = () => {
     let username = document.getElementById('unameSI').value;
     let psswrd = document.getElementById('pswrdSI').value;
-
-    if (psswrd == "" || username == ""){
-        document.getElementById('alerts').value = "invalid username or password, please try again";
+    let snackMsg = document.getElementById('snackbar');
+    
+    if (psswrd == "" || username == "") {
+        //snackMsg.value = "Login failed, please confirm your login and password";
+        snackMsg.innerHTML = "Error! Can't login, invalid username/password, please try again!";
+        snackMsg.className = "show";
+        setTimeout(function(){ snackMsg.className = snackMsg.className.replace("show", ""); }, 3000);
     }
     else {
-        document.getElementById('alerts').value = "Welcome!";
+        snackMsg.innerHTML = "Login Successful!";
+        snackMsg.className = "show";
+        setTimeout(function(){ snackMsg.className = snackMsg.className.replace("show", ""); }, 3000);
     }
 }
 
@@ -14,14 +20,22 @@ document.getElementById('createAcc_btn').onclick = () => {
     let username = document.getElementById('unameSU').value;
     let psswrd = document.getElementById('pwrdSU').value;
     let psswrdCon = document.getElementById('pwrdConSU').value;
-   
+    let snackMsg = document.getElementById('snackbar');
+
     if (psswrd == "" || username == "" || psswrdCon == "") {
-        document.getElementById('alerts').value = "invalid username or password, please try again";
+        snackMsg.innerHTML = "Error! Can't create account, invalid username/password"
+        snackMsg.className = "show";
+        setTimeout(function(){ snackMsg.className = snackMsg.className.replace("show", ""); }, 3000);
     }
     else if (psswrd != psswrdCon) {
-        document.getElementById('alerts').value = "Your password entries do not match please try again!";
+        snackMsg.innerHTML = "Your new password entries do not match please try again!";
+        snackMsg.className = "show";
+        setTimeout(function(){ snackMsg.className = snackMsg.className.replace("show", ""); }, 3000);
     }
     else {
-        document.getElementById('alerts').value = "Welcome! Thanks for signing up";
+        snackMsg.innerHTML = "Account Created Successfully";
+        snackMsg.className = "show";
+        setTimeout(function(){ snackMsg.className = snackMsg.className.replace("show", ""); }, 3000);
     }
 }
+
